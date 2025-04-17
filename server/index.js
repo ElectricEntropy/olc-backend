908b08f8bebf6ad76d447a2037ef34a028ba8b1c
ElectricEntropy <=>
2025-04-16 20:48:04 -0400

    sync api routes with front end

--- server/index.js
+++ server/index.js
@@ -8,9 +8,10 @@ app.use(express.json());
 
 //customers
 //create
-app.post("/customers", async (req, res) => {
+app.post("/api/customers", async (req, res) => {
   try {
-    const { first_name, last_name, phone_number, email, birth_date, comic_discount, store_credit, account_creation_date, notes } = req.body;
+    const { first_name, last_name, phone_number, email, birth_date, comic_discount, store_credit, notes } = req.body;
+    const account_creation_date = new Date().toLocaleString();
 
     const newCustomer = await pool.query(
       "INSERT INTO customers VALUES(DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
@@ -20,11 +21,12 @@ app.post("/customers", async (req, res) => {
     res.json(newCustomer.rows[0]);
   } catch (error) {
     console.error(error.message);
+    res.status(400).send(error.message);
   }
 })
 
 //return all customers
-app.get("/customers", async (req, res) => {
+app.get("/api/customers", async (req, res) => {
   try {
     const allCustomers = await pool.query("SELECT * FROM customers");
 
@@ -34,7 +36,7 @@ app.get("/customers", async (req, res) => {
   }
 })
 
-app.get("/customers/:id", async (req, res) => {
+app.get("/api/customers/:id", async (req, res) => {
   try {
     const { id } = req.params;
 
@@ -43,40 +45,29 @@ app.get("/customers/:id", async (req, res) => {
     res.json(customer.rows[0]);
   } catch (error) {
     console.error(error.message);
-  }
-})
-
-//get all comic pulls from a customer id
-app.get("/customers/:id/comic-pulls", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const comicPulls = await pool.query("SELECT * FROM comic_pull_list WHERE customer_id = $1", [id]);
-
-    res.json(comicPulls.rows);
-  } catch (error) {
-    console.error(error.message);
+    res.status(400).send(error.message);
   }
 })
 
 //update
-app.put("/customers/:id", async (req, res) => {
+app.put("/api/customers/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const { first_name, last_name, phone_number, email, birth_date, comic_discount, store_credit, account_creation_date, notes } = req.body;
+    const { first_name, last_name, phone_number, email, birth_date, comic_discount, store_credit, notes } = req.body;
 
-    const customer = await pool.query("UPDATE customers SET first_name = $2, last_name = $3, phone_number = $4, email = $5, birth_date = $6, comic_discount = $7, store_credit = $8, account_creation_date = $9, notes = $10 WHERE id = $1 RETURNING *",
-      [id, first_name, last_name, phone_number, email, birth_date, comic_discount, store_credit, account_creation_date, notes]
+    const customer = await pool.query("UPDATE customers SET first_name = $2, last_name = $3, phone_number = $4, email = $5, birth_date = $6, comic_discount = $7, store_credit = $8, notes = $9 WHERE id = $1 RETURNING *",
+      [id, first_name, last_name, phone_number, email, birth_date, comic_discount, store_credit, notes]
     );
 
     res.json(customer.rows[0]);
   } catch (error) {
     console.error(error.message);
+    res.status(400).send(error.message);
   }
 })
 
 //delete
-app.delete("/customers/:id", async (req, res) => {
+app.delete("/api/customers/:id", async (req, res) => {
   try {
     const { id } = req.params;
 
@@ -85,66 +76,159 @@ app.delete("/customers/:id", async (req, res) => {
     res.json("Deleted");
   } catch (error) {
     console.error(error.message);
+    res.status(400).send(error.message);
   }
 })
 
 
-//comic pull list
+//comics
 //create
-app.post("/comic-pull-list", async (req, res) => {
+app.post("/api/comics", async (req, res) => {
   try {
-    const { customer_id, comic_title, issue_number, custom_item } = req.body;
+    const { title, issue_number, publisher, is_custom } = req.body;
+    const added_at = new Date().toLocaleString();
+
+    const newComic = await pool.query(
+      "INSERT INTO comics VALUES(DEFAULT, $1, $2, $3, $4, $5) RETURNING *",
+      [title, issue_number, publisher, added_at, is_custom]
+    );
+
+    res.json(newComic.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).send(error.message);
+  }
+})
+
+//return all comics
+app.get("/api/comics", async (req, res) => {
+  try {
+    const allComics = await pool.query("SELECT * FROM comics");
+
+    res.json(allComics.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+})
+
+app.get("/api/comics/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const comic = await pool.query("SELECT * FROM comics WHERE id = $1", [id]);
+
+    res.json(comic.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).send(error.message);
+  }
+})
+
+//update
+app.put("/api/comics/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, issue_number, publisher, is_custom } = req.body;
+
+    const comic = await pool.query("UPDATE comics SET title = $2, issue_number = $3, publisher = $4, is_custom = $5 WHERE id = $1 RETURNING *",
+      [id, title, issue_number, publisher, is_custom]
+    );
+
+    res.json(comic.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).send(error.message);
+  }
+})
+
+//delete
+app.delete("/api/comics/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deleteComic = await pool.query("DELETE FROM comics WHERE id = $1", [id]);
+
+    res.json("Deleted");
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).send(error.message);
+  }
+})
+
+//comic pulls
+//create
+app.post("/api/customers/:id/pulls", async (req, res) => {
+  try {
+    const customer_id = req.params.id;
+    const comic_id = req.body.id;
+    const date_added = new Date().toLocaleString();
 
     const newComicPulls = await pool.query(
-      "INSERT INTO comic_pull_list VALUES(DEFAULT, $1, $2, $3, $4) RETURNING *",
-      [customer_id, comic_title, issue_number, custom_item]
+      "INSERT INTO pulls VALUES(DEFAULT, $1, $2, $3) RETURNING *",
+      [customer_id, comic_id, date_added]
     );
 
     res.json(newComicPulls.rows[0]);
   } catch (error) {
     console.error(error.message);
+    res.status(400).send(error.message);
   }
 })
 
-//return
-app.get("/comic-pull-list", async (req, res) => {
+//get all comic pulls from a customer id
+app.get("/api/customers/:id/pulls", async (req, res) => {
   try {
-    const comicPulls = await pool.query("SELECT * FROM comic_pull_list");
+    const { id } = req.params;
+
+    const comicPulls = await pool.query("SELECT * FROM pulls WHERE customer_id = $1", [id]);
 
     res.json(comicPulls.rows);
   } catch (error) {
     console.error(error.message);
+    res.status(400).send(error.message);
   }
 })
 
+//return all pulls
+app.get("/api/pulls", async (req, res) => {
+  try {
+    const comicPulls = await pool.query("SELECT * FROM pulls");
 
+    res.json(comicPulls.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).send(error.message);
+  }
+})
 
 //update
-app.put("/comic-pull-list/:id", async (req, res) => {
+app.put("/api/customers/:id/pulls", async (req, res) => {
   try {
     const { id } = req.params;
-    const { customer_id, comic_title, issue_number, custom_item } = req.body;
+    const { customer_id, comic_id } = req.body;
 
-    const comic_pulls = await pool.query("UPDATE comic_pull_list SET customer_id = $2, comic_title = $3, issue_number = $4, custom_item = $5 WHERE id = $1 RETURNING *",
-      [id, customer_id, comic_title, issue_number, custom_item]
+    const comic_pulls = await pool.query("UPDATE pulls SET customer_id = $2, comic_id = $3 WHERE id = $1 RETURNING *",
+      [id, customer_id, comic_id]
     );
 
     res.json(comic_pulls.rows[0]);
   } catch (error) {
     console.error(error.message);
+    res.status(400).send(error.message);
   }
 })
 
-//delete
-app.delete("/comic-pull-list/:id", async (req, res) => {
+//delete pull
+app.delete("/api/pulls/:id", async (req, res) => {
   try {
     const { id } = req.params;
 
-    const deletePull = await pool.query("DELETE FROM comic_pull_list WHERE id = $1", [id]);
+    const deletePull = await pool.query("DELETE FROM pulls WHERE id = $1", [id]);
 
     res.json("Deleted");
   } catch (error) {
     console.error(error.message);
+    res.status(400).send(error.message);
   }
 })
 
